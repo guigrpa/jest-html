f1c50374d5866906dcfd4ef20eff05f007d0edd6
G. Grau <guigrpa@gmail.com>
2016-11-01 23:01:11 +0100

    v0.3.2

--- package.js
+++ package.js
@@ -6,7 +6,7 @@
 // Basic config
 // ===============================================
 const NAME = 'jest-html';
-const VERSION = '0.3.1';
+const VERSION = '0.3.2';
 const DESCRIPTION = 'Preview Jest snapshots right in your browser';
 const KEYWORDS = ['Jest', 'test', 'snapshot', 'serializer', 'html', 'preview'];
 
