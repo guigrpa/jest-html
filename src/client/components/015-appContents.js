b24bc7f43fb3e6af84690faa0144f3893a2c9831
G. Grau <ggrau@indra.es>
2016-11-02 10:30:16 +0100

    Fix sidebar CSS

--- src/client/components/015-appContents.js
+++ src/client/components/015-appContents.js
@@ -227,7 +227,7 @@ class AppContents extends React.PureComponent {
 // ------------------------------------------
 const style = {
   outer: flexContainer('row', {
-    height: '100vh',
+    minHeight: '100vh',
   }),
   titleBarIcon: {
     cursor: 'default',
