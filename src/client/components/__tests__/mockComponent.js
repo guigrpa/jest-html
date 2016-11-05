d6e8b40e8838780c6c57dbbaf0e8e51663033f43
G. Grau <guigrpa@gmail.com>
2016-11-05 15:41:34 +0100

    Tests: show mocks more clearly

--- src/client/components/__tests__/mockComponent.js
+++ src/client/components/__tests__/mockComponent.js
@@ -1,5 +1,18 @@
 const React = require('react');
 
 module.exports = (name) => (props) => (
-  <div dataMockType={name} {...props}>{props.children || name}</div>
+  <div dataMockType={name} {...props}>
+    <div style={{ fontWeight: 'bold', color: 'blue' }}>{name}</div>
+    {props.children || name}
+  </div>
 );
+
+// const renderProps = (props) => {
+//   if (!props) return null;
+//   const out = [];
+//   Object.keys(props).forEach((key) => {
+//     if (key === 'children') return;
+//     out.push(<div key={key}>{key} = {props[key]}</div>);
+//   });
+//   return out;
+// };
